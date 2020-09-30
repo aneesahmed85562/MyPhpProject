7db0c2896b7cab8d6046eb26308a383a9ef274c1
aneesahmed85562 <72161074+aneesahmed85562@users.noreply.github.com>
2020-10-01 00:16:03 +0500

    Changed

--- db.php
+++ db.php
@@ -1,3 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="bootstrap-4.4.1-dist\css\bootstrap.min.css" rel="stylesheet"/>
+</head>
+
+<body>
 <?php
 
 $host= "localhost";
@@ -16,23 +25,32 @@ $query = "select * from help_category";
 $result = mysqli_query($con,$query);
 ?>
 
-	<table border="1px">
+	<table border="1px" class="table">
+
+	<thead class="thead-dark">
 	<tr>
-	<th>ID</th>
-	<th>Name</th>
-	<th>Parent Category ID</th>
+	<th scope="col">ID</th>
+	<th scope="col">Name</th>
+	<th scope="col"> Parent Category ID</th>
 	</tr>
+	</thead>
+		<tbody>
 	<?php
 		while($row= mysqli_fetch_assoc($result))
 		{
 		?>
 			<tr>
-				<td> <?php echo $row['help_category_id'] ?> </td>
+				<td scope="row"> <?php echo $row['help_category_id'] ?> </td>
 				<td> <?=$row['name']?></td>
 				<td> <?=$row['parent_category_id']?> </td>
 			</tr>
 		<?php
-		}?>
+		} ?>
+		</tbody>
 	</table>
 
-	
+	</body>
+
+</html>
+<script src="C:\xampp\htdocs\MyPhpProject\bootstrap-4.4.1-dist\js\Jquery.js"></script>
+<script src="C:\xampp\htdocs\MyPhpProject\bootstrap-4.4.1-dist\js\bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Untitled Document</title>
+<link href="bootstrap-4.4.1-dist\css\bootstrap.min" rel="stylesheet"/>
 </head>
 
 <body>
@@ -11,9 +12,11 @@
 	<br>
 	<input type="text" name="user">
 	<br>
-	<input type="submit">
+	<input type="submit" class="btn btn-primary">
 </form>
 	
 </body>
 
 </html>
+<script src="bootstrap-4.4.1-dist\js\Jquery.min"></script>
+<script src="bootstrap-4.4.1-dist\js\js.min"></script>
